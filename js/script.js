5feece86e04044056454e88da12b71edf775739e
Paulina Stefanska <web.paulinastefanska@gmail.com>
2019-06-17 11:48:29 +0200

    Fix small issue

--- js/script.js
+++ js/script.js
@@ -4,12 +4,15 @@ function Button(text) {
 
 Button.prototype = {
 	create: function() {
-		this.element = document.createElement('button');
-		this.element.innerText = this.text;
-		this.element.addEventListener('click', function() {
-			alert(self.text);
-		});
-		document.body.appendChild(this.element);
+	var self = this;
+	this.element = document.createElement('button');
+	this.element.innerText = this.text;
+	this.element.addEventListener('click', function() {
+		alert(self.text);
+	});
+	document.body.appendChild(this.element);
 	}
 }
 
+var btn1 = new Button('Hello!');
+btn1.create();
